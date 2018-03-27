67b61327cd079454d9707169299f94f5a75ed273
gdnyfcuso <gdnyfcuso@163.com>
2018-03-27 19:11:45 +0800

    提交代码

---
+++ CloudTraceability.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CloudTraceability.Web
+{
+    public class BundleConfig
+    {
+        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
+            // 生产准备就绪，请使用 https://modernizr.com 上的生成工具仅选择所需的测试。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap-4.0.0/dist/js/bootstrap.min.js",
+                      "~/Scripts/respond.js", "~/Scripts/bootstrap-4.0.0/assets/js/vendor/popper.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Scripts/bootstrap-4.0.0/dist/css/bootstrap.min.css"));
+        }
+    }
+}
