e42f5eb8b4dae4a6e78be7ace109061d50dfcbb8
gdnyfcuso <gdnyfcuso@163.com>
2018-03-29 21:39:03 +0800

    用户验证

--- CloudTraceability.Web/Scripts/CloudTraceabilityJS/Login/js/login.js
+++ CloudTraceability.Web/Scripts/CloudTraceabilityJS/Login/js/login.js
@@ -1,8 +1,29 @@
 ﻿$(document).ready(function () {
+    RegitClass();
+    RegitSumit();
+    
+});
+
+function RegitSumit()
+{
+    $("#mySubmit").click(function (event) {
+        var userName = $("#UserName").val();
+
+        var password = $("#Password").val();
+        
+        $.Ajax.post("Account/Login", { userName: userName, password :password}, function (result) {
+            window.location = result;
+        });
+
+    });
+}
+
+function RegitClass()
+{
     $('.forgot-pass').click(function (event) {
         $(".pr-wrap").toggleClass("show-pass-reset");
     });
     $('.pass-reset-submit').click(function (event) {
         $(".pr-wrap").removeClass("show-pass-reset");
     });
-});
+}
